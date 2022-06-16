ac8f50d7572f97dbe5fdb2b25c1ddfeeca399f6e
Focusm3n <ilya.k007123@gmail.com>
2022-06-17 00:27:41 +0300

    blade 2.0

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,12 +1,14 @@
 @extends('layout.app')
-@section('title', 'Главная страница')
+@section('title', 'Статьи')
 @section('content')
 
-@include('partials.header')
+    @include('partials.header')
     <div class="grid grid-cols-1 md:grid-cols-3 gap-10 mt-10 mb-20">
         @foreach($posts as $post)
             @include('posts.partials.item')
         @endforeach
+
+        {{ $posts->links() }}
     </div>
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 //Route::middleware("r")->group(function (){
 //    Route::get('/', [\App\Http\Controllers\IndexController::class, 'index'])->name('home');
 //});
 
 Route::get('/', [\App\Http\Controllers\IndexController::class, 'index'])->name('home');
+Route::get('/posts', [\App\Http\Controllers\PostController::class, 'index'])->name('posts.index');
+Route::get('/posts{id}', [\App\Http\Controllers\PostController::class, 'show'])->name('posts.show');

88b256371936000b15e8ca2c04b9386da654b0ab
Focusm3n <ilya.k007123@gmail.com>
2022-06-16 15:19:02 +0300

    routes and middleware

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware("r")->group(function (){
+    Route::get('/', [\App\Http\Controllers\IndexController::class, 'index'])->name('home');
 });
+
